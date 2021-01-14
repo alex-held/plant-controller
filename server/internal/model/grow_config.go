e13f6054c4798881a99d52140a44121df3d123c5
alex held <git@alexheld.io>
2021-01-14 07:23:59 +0100

    feat(server): add growconfig

--- server/internal/model/grow_config.go
+++ server/internal/model/grow_config.go
@@ -1,1 +1,9 @@
 package model
+
+type GrowConfig struct {
+	Id int `json:"id"`
+	PlantType PlantType `json:"plant_type"`
+	Name string      `json:"name"`
+	GerminationDays int `json:"germination_days"`
+	GrowingDays int `json:"growing_days"`
+}
